882157fd42e70962a4d28d5f654471b2ad2f5c45
Uditp11 <uditparihar11@gmail.com>
2024-11-26 20:58:59 +0530

    0-4 Create application

--- src/main/java/com/ecomweb/online/da/DA/HelloWorld.java
+++ src/main/java/com/ecomweb/online/da/DA/HelloWorld.java
@@ -1,9 +1,11 @@
 package com.ecomweb.online.da.DA;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloWorld {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String funcHello(){
         return "Hello World!!";
     }
