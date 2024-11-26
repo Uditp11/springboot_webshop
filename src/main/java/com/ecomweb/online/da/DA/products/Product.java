ee460a59cebcb4eb0a536f2797e65439d4c4a102
Uditp11 <uditparihar11@gmail.com>
2024-11-26 20:22:05 +0530

    0-4 Create application

---
+++ src/main/java/com/ecomweb/online/da/DA/products/Product.java
@@ -0,0 +1,43 @@
+package com.ecomweb.online.da.DA.products;
+
+public class Product {
+    private int id;
+    private String name;
+    private String type;
+    private double price;
+    private String size;
+    private String color;
+
+    public Product(int id, String name, String type, double price, String size, String color) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.price = price;
+        this.size = size;
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
